d64ffa7814037bf8161c3488e08e60cd9b2a1fff
tkeena <tarakeena12@gmail.com>
2014-06-21 13:04:14 +0100

    Initialize repository

---
+++ railsgirls-galway-2014/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsgirlsGalway2014::Application.config.secret_key_base = '15e487264ed2b5c73384aa116955ee620e07df155674ed6beaa530fa7086636b87dc184cdffea2d6ae809d51c61cd4393efd52eef65ce31e67558fdcc3a3f1bc'
---
+++ railsgirls-galway-2014/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsgirlsGalway2014::Application.config.session_store :cookie_store, key: '_railsgirls-galway-2014_session'
